45722ab83d886a29c184155982fc533aca7db42e
Dawson Antonio Buffo <dawsonabuffobusiness@gmail.com>
2024-12-08 23:06:34 -0600

    made app.js to dsiplay the app

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import AddStudent from "./components/AddStudent.js";
+import Header from "./components/Header.js";
+import StudentList from "./components/StudentList.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  Header();
+  AddStudent();
+  StudentList();
+});
